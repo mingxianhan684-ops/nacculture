8a87a5435e1d233181b8fdaaf97f06e5e709f5ec
mingxianhan684-ops <mingxianhan684@gmail.com>
2026-06-17 00:25:43 -0500

    Update slider.js

--- js/slider.js
+++ js/slider.js
@@ -1,1 +1,32 @@
-// Homepage Slider
+// NACCF Homepage Slider
+
+const slides = document.querySelectorAll(".hero-slide");
+const dots = document.querySelectorAll(".slider-dot");
+
+let currentSlide = 0;
+
+function showSlide(index){
+  slides.forEach(slide => slide.classList.remove("active"));
+  dots.forEach(dot => dot.classList.remove("active"));
+
+  slides[index].classList.add("active");
+  dots[index].classList.add("active");
+
+  currentSlide = index;
+}
+
+function nextSlide(){
+  currentSlide++;
+  if(currentSlide >= slides.length){
+    currentSlide = 0;
+  }
+  showSlide(currentSlide);
+}
+
+dots.forEach((dot,index)=>{
+  dot.addEventListener("click",()=>{
+    showSlide(index);
+  });
+});
+
+setInterval(nextSlide,5000);
